14cd72b9683b2fda0ccd37ca0798db5b56e70e8c
k-murakami0609 <dadgac@gmail.com>
2020-10-11 12:21:55 +0900

    lazy allocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -57,6 +57,22 @@ void usertrap(void) {
     syscall();
   } else if ((which_dev = devintr()) != 0) {
     // ok
+  } else if (r_scause() == 13 || r_scause() == 15) {
+    uint64 va = r_stval();
+    if (va >= p->sz) {
+      exit(-1);
+    }
+    char *mem = kalloc();
+    if (mem == 0) {
+      exit(-1);
+    }
+    memset(mem, 0, PGSIZE);
+
+    va = PGROUNDDOWN(va);
+    if (mappages(p->pagetable, va, PGSIZE, (uint64)mem,
+                 PTE_W | PTE_X | PTE_R | PTE_U) != 0) {
+      exit(-1);
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
